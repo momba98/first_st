73307fb3d5f9c4cf971b21555acc30928ff999da
Andre Mombach <1998amds@gmail.com>
2021-01-24 20:58:09 -0300

    update_24_01_21

--- first_st.py
+++ first_st.py
@@ -1,15 +1,18 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
+import os
 import xlrd
 
-st.set_page_config(page_title='Nome na aba', page_icon="https://static.streamlit.io/examples/cat.jpg", layout='wide', initial_sidebar_state='auto')
+#falta construir o ranking e tentar otimizar essa exibição dos floats. no futuro, automatizar push pro github junto com o DDE pra não precisar ficar fazendo isso
+
+st.set_page_config(page_title='Nome na aba', page_icon="https://static.streamlit.io/examples/cat.jpg", layout='centered', initial_sidebar_state='auto')
 
 st.title("""
 
-HELLO MYFRIEND
+BOLSISTAS DATA ANALYSIS
 
 """)
 
@@ -20,18 +23,126 @@ if agree:
 
 st.write("""
 
-## Esse é o título
+    ## VAMOS ESTUDAR OS VOLUMES:
+    ### QUANTIDADE, VALOR E NEGÓCIOS
+
 
 """)
 
+displaying = st.radio(
+    label = "Como você gostaria de visualizar as informações?",
+    options = ('Diário', 'Range Diário'),
+    index=1
+    )
+
 #displaying um df
+def show_daily():
 
-df = pd.read_excel('2021-01-19.xlsx')
+    armz = {}
 
-df.drop(df.index[80],inplace=True) #excluindo o IBOV
-df.drop(df.columns[0], axis=1, inplace=True) #excluindo aquela primeira coluna podre
+    for file in os.listdir("planilhas_diarias"):
+        if file.endswith(".xlsx"):
+            df = pd.read_excel(f'planilhas_diarias/{file}')
+            df.drop(df.index[80],inplace=True) #excluindo o IBOV
 
-st.dataframe(df) #poderia ser st.write(df), mesma coisa. .table mostra ela estática
+            df = df[['Asset','Nome do Ativo','Variação','Negócios', 'Quantidade', 'Volume']]
+
+            armz[file[:-5]] = df
+
+    del armz['fonte_DDE']
+
+    show_df = st.selectbox(
+         'Qual dia você gostaria de visualizar as informações?',
+         list(armz))
+
+    st.dataframe(armz[show_df]) #poderia ser st.write(df), mesma coisa. .table mostra ela estática
+
+def show_range_daily():
+
+    displaying2 = st.radio(
+        label = "Como você gostaria de analisar as informações?",
+        options = ('Em soma', 'Em média'),
+        index=0
+        )
+
+    armz = {}
+
+    for file in os.listdir("planilhas_diarias"):
+        if file.endswith(".xlsx"):
+            df = pd.read_excel(f'planilhas_diarias/{file}')
+            df.drop(df.index[80],inplace=True) #excluindo o IBOV
+            df.drop(df.index[22],inplace=True) #excluindo o BOVA11
+
+            df = df[['Asset','Variação','Negócios', 'Quantidade', 'Volume']]
+
+            armz[file[:-5]] = df
+
+    del armz['fonte_DDE']
+
+    Y_i=int(list(armz)[0][:4])
+    M_i=int(list(armz)[0][5:7])
+    D_i=int(list(armz)[0][8:10])
+
+    data_i=datetime(Y_i,M_i,D_i)
+
+    Y_f=int(list(armz)[-1][:4])
+    M_f=int(list(armz)[-1][5:7])
+    D_f=int(list(armz)[-1][8:10])
+
+    data_f=datetime(Y_f,M_f,D_f)
+
+    show_df = st.slider(
+        label='Qual range você gostaria de visualizar?',
+        min_value=data_i,
+        max_value=data_f,
+        value=[data_i,data_f])
+        #format="YY-MM-DD")
+
+    qtd_dias = str(show_df[1]-show_df[0] + timedelta(days=1)).split(',')[0]
+
+    st.write('We are talking about', qtd_dias)
+
+    day_low = str((show_df)[0])[:-9]
+    day_high = str((show_df)[1])[:-9]
+
+    lista_inicio_fim_iteracao=[]
+
+    for chave,valor in enumerate(armz.keys()):
+        if valor == day_low:
+            lista_inicio_fim_iteracao.append(chave)
+
+        if valor == day_high:
+            lista_inicio_fim_iteracao.append(chave)
+
+            #duplo é necessário pra lista ter sempre o mesmo tamanho
+
+    dfs = armz[day_low].copy()
+
+    for somador in range(lista_inicio_fim_iteracao[0]+1,lista_inicio_fim_iteracao[1]+1):
+        dfs += armz[list(armz)[somador]]
+
+    if displaying2 == 'Em média':
+
+        if qtd_dias[-1] == 's':
+            dias = int(qtd_dias[:-5])
+        else:
+            dias = 1
+
+        dfs[['Variação','Negócios', 'Quantidade', 'Volume']] = round(dfs[['Variação','Negócios', 'Quantidade', 'Volume']]/dias,2) #retirando o ' days'
+
+        #dfs[['Volume']] = dfs[['Volume']].astype(float)
+        dfs[['Negócios', 'Quantidade']] = dfs[['Negócios', 'Quantidade']].astype(int)
+
+    dfs[['Asset']] = armz[day_low][['Asset']]
+
+    st.write(dfs) #poderia ser st.write(df), mesma coisa. .table mostra ela estática
+
+
+
+if displaying == 'Diário':
+     show_daily()
+elif displaying == 'Range Diário':
+     show_range_daily()
 
 #st.line_chart(df.Abertura)
 
@@ -39,28 +150,8 @@ st.dataframe(df) #poderia ser st.write(df), mesma coisa. .table mostra ela está
 
 st.image(Image.open('vaito.jpg'), caption='VAAAI TOURINHO', use_column_width=True) #USANDO O PIL, ABRIR UMA IMG
 
-genre = st.radio("What's your favorite movie genre", ('Comedy', 'Drama', 'Documentary'))
-
-if genre == 'Comedy':
-     st.write('You selected comedy.')
-else:
-     st.write("You didn't select comedy.")
-
-
-option = st.selectbox(
-     'How would you like to be contacted?',
-     ('Email', 'Home phone', 'Mobile phone'))
-
-st.write('You selected:', option)
-
 age = st.slider('How old are you?', 0, 130, 27) #para transformar em um range, apenas colocar o 3o argumento como lista
 st.write("I'm ", age, 'years old')
-
-start_time = st.slider(
-     "When do you start?",
-     value=datetime(2020, 1, 1, 9, 30),
-     format="MM/DD/YY - hh:mm")
-st.write("Start time:", start_time)
 
 
 #title = st.text_input('Movie title', 'Life of Brian')  #or it could be number_input too
@@ -69,10 +160,11 @@ st.write("Start time:", start_time)
 #a sidebar dispõe as opções num menu ao lado pra dar mais foco  à informação.
 # a key serve pra referenciar esse cara.
 
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone"), key='sfw121'
-)
+#add_selectbox = st.sidebar.selectbox(
+#    "How would you like to be contacted?",
+#    ("Email", "Home phone", "Mobile phone"), key='sfw121'
+#)
+
 
 #beta_columns organiza o app em forma de colunas.
 # se for passado só um int, é o número de colunas que vai ter, todas iguais.
@@ -107,13 +199,13 @@ with st.beta_expander("See explanation"):
      """)
      st.image("https://static.streamlit.io/examples/dice.jpg")
 
-st.balloons()
+#st.balloons()
 
-st.error('This is an error')
+#st.error('This is an error')
 
-st.info('This is a purely informational message')
+#st.info('This is a purely informational message')
 
-st.success('This is a success message!')
+#st.success('This is a success message!')
 
 #Inserts a container into your app that can be used to hold a single element.
 
---
+++ planilhas_diarias/abridor.py
@@ -0,0 +1,46 @@
+import datetime
+import os
+import time
+from pynput.keyboard import Key, Controller
+import pandas as pd
+
+#abrir o profit
+os.system("start C:/Users/1998a/AppData/Roaming/Nelogica/ClearTrader/profitchart.exe")
+
+#esperar carregar, conectar, se estabelecer
+time.sleep(15)
+
+#abrir planilha atrelada ao DDE do profit
+os.chdir("C:/Users/1998a/Documents/GitHub/bolsistas_da/planilhas_diarias")
+
+os.system("start EXCEL.EXE fonte_DDE.xlsx")
+
+#esperar carregar
+time.sleep(10)
+
+#apertar ENTER para autorizar a atualização dos dados
+keyboard = Controller()
+keyboard.press(Key.enter)
+time.sleep(2)
+keyboard.release(Key.enter)
+
+#carregar todos os dados
+time.sleep(10)
+
+#salvar
+keyboard.press(Key.ctrl)
+keyboard.press('b')
+time.sleep(1)
+keyboard.release('b')
+keyboard.release(Key.ctrl)
+
+time.sleep(2)
+
+#fechar ambos
+os.system('TASKKILL /F /IM excel.exe')
+os.system('TASKKILL /F /IM profitchart.exe')
+
+#usar pandas pra ler a tabela atualiazda e salvar em outro arquivo com o nome de hoje
+df = pd.read_excel("fonte_DDE.xlsx")
+today = datetime.date.today()
+df.to_excel(f"{today}.xlsx")
